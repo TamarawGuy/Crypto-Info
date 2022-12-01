5ba03f40dc9bc8be962246142fa546d2e178ac7b
TamarawGuy <viktor.m.rumenov@gmail.com>
2022-12-01 19:25:36 +0200

    Styled navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Menu, Image } from "antd";
+import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <Menu mode="horizontal">
-      <Menu.Item>
-        <Link href="/">Home</Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link href="/cryptocurrencies">CryptoCurrencies</Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link href="/">Home</Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link href="/">Home</Link>
-      </Menu.Item>
-    </Menu>
+    <nav className={styles.navContainer}>
+      <div className={styles.logoContainer}>
+        <img
+          className={styles.logoContainerImage}
+          src="images/icon.png"
+          alt="logo"
+        />
+      </div>
+      <div className={styles.menuContainer}>
+        <Menu theme="dark" mode="horizontal">
+          <Menu.Item>
+            <Link href="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="/cryptocurrencies">CryptoCurrencies</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      </div>
+    </nav>
   );
 };
 
